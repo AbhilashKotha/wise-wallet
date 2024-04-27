e6fa7596a90427fbc324abda4341e18acb7301da
SiriChandanaGarimella <156231102+SiriChandanaGarimella@users.noreply.github.com>
2024-04-27 15:37:49 -0500

    added dashboard

--- client-app/src/App.js
+++ client-app/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import PersonalDataChart from './components/PersonalDataChartComponent';
+import Dashboard from './components/DashboardComponent';
 import AboutComponent from './components/AboutComponent';
 
 const App = () => {
@@ -10,7 +10,7 @@ const App = () => {
     <Router>
       <NavBar/>
       <Routes>
-        <Route path="/dashboard" element={<PersonalDataChart />} />
+        <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/" element={<Navigate to="/dashboard" replace />} />
         <Route path="/about" element={<AboutComponent />} />
       </Routes>
