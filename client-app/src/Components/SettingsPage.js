c853f24e5ca2939516ac2cdeff7ff01130e885b8
Abhilash Kotha <abhilash.kotha@slu.edu>
2024-04-28 08:30:04 -0500

    settings update

--- client-app/src/Components/SettingsPage.js
+++ client-app/src/Components/SettingsPage.js
@@ -1,13 +1,28 @@
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 import {Col,Button} from 'react-bootstrap';
+import { useState } from 'react'; 
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
 
 function SettingsPage() {
-    const personData = {firstName: "Abhilash",
-lastName:"Kotha",
-email:"akotha1@slu.edu",
+
+  const [selectedMonth, setSelectedMonth] = useState(null);
+  const [selectedAmount, setSelectedAmount] = useState('');
+
+  const handleMonthSelect = (month) => {
+    setSelectedMonth(month);
+    const selectedBudget = personData.MonthlyBudgets.find(budget => budget.Month === month);
+    setSelectedAmount(selectedBudget ? selectedBudget.amount : '');
+  };
+    const personData = {firstName: "John",
+lastName:"Deo",
+email:"jdka1@slu.edu",
 emailNotif: true,
-Banks: [{name:"US Bank",lastFour:"3232"},{name:"Chase",lastFour:"6632"}]}
+Banks: [{name:"US Bank",lastFour:"3232"},{name:"Chase",lastFour:"6632"}],
+MonthlyBudgets: [{Month:"April",amount:"$1200"},{Month:"May",amount:"$2000"},{Month:"June",amount:"800"}],
+YearBudget: "$20000"}
+
     return (
       <>
       <Col xs={6} md={4} style={{marginLeft:"5em"}}> 
@@ -63,7 +78,31 @@ Banks: [{name:"US Bank",lastFour:"3232"},{name:"Chase",lastFour:"6632"}]}
 }
         </>):<><p>No bank accounts are linked</p></>}
       
-     
+        <h3 style={{marginTop:"30px"}}>Budget Information</h3>
+
+      <InputGroup className="mb-3">
+        <DropdownButton
+          variant="outline-secondary"
+          title={selectedMonth || "Dropdown"}
+          id="input-group-dropdown-1"
+        >
+           {personData.MonthlyBudgets.map((month) => (
+          <Dropdown.Item key={month.Month} onClick={() => handleMonthSelect(month.Month)}>{month.Month}</Dropdown.Item>
+           ))}
+        </DropdownButton>
+        <Form.Control aria-label="Text input with dropdown button" 
+        defaultValue={selectedAmount}
+        />
+      </InputGroup>
+      <InputGroup className="mb-3">
+        <InputGroup.Text id="basic-addon1" style={{width:"7em"}}>2024</InputGroup.Text>
+        <Form.Control
+          placeholder="Enter Your Yearly Budget"
+          aria-label="yeatbudget"
+          aria-describedby="basic-addon1"
+          defaultValue = {personData.YearBudget}
+        />
+      </InputGroup>
       </Col>
       </>
     )
